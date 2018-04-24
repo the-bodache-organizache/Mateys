1accddf33da8138d7504a13c9838cae3f46ee6c3
Thomas Kelly <tmkelly28@gmail.com>
2018-04-24 10:32:12 -0400

    rename to appy mcappface

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Welcome = () => {
-  return <h1>Welcome to Boilermaker!</h1>
+  return <h1>Welcome to Appy McAppFace!</h1>
 }
 
 export default Welcome
--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,13 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/grace-shopper', {
+const pkg = require('../../package.json')
+const defaultDatabaseUrl = `postgres://localhost/${pkg.name}`
+
+let dbName = process.env.DATABASE_URL || defaultDatabaseUrl
+if (process.env.NODE_ENV === 'test') {
+  dbName += '-test'
+}
+
+const db = new Sequelize(dbName, {
   logging: false
 })
 
