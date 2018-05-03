1c6508f938be4410c683ae4f9111d8b2b15b9331
Tim Carleton <carletontimothy@gmail.com>
2018-05-03 16:28:43 -0400

    cant draw video

--- client/components/test/MotionDetection.js
+++ client/components/test/MotionDetection.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame    ||
+    window.oRequestAnimationFrame      ||
+    window.msRequestAnimationFrame     ||
+    function( callback ){
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
 class MotionDetection extends React.Component {
   constructor() {
     super();
@@ -7,8 +18,7 @@ class MotionDetection extends React.Component {
     this.canvasBlended = (
       <canvas id="canvas-blended" width="640" height="480" />
     );
-    //this.contextSource = this.canvasSource.getContext('2d');
-    //this.contextBlended = this.canvasBlended.getContext('2d');
+    
     this.video = (
       <video
         autoPlay="autoplay"
@@ -90,11 +100,89 @@ class MotionDetection extends React.Component {
   };
 
   drawVideo = () => {
-    this.contextSource.drawImage(this.video);
+    console.log(this.canvasSource, 'sourcelog')
+    let contextSource = this.canvasSource.getContext('2d');
+    contextSource.drawImage(this.video);
   };
+
+  blend = () => {
+    let width = this.canvasSource.width;
+    let height = this.canvasSource.height;
+    let contextSource = this.canvasSource.getContext('2d');
+    let contextBlended = this.canvasBlended.getContext('2d');
+    let sourceData = contextSource.getImageData(0, 0, width, height);
+		// create an image if the previous image doesn’t exist
+		if (!lastImageData) lastImageData = this.contextSource.getImageData(0, 0, width, height);
+		// create a ImageData instance to receive the blended result
+		let blendedData = contextSource.createImageData(width, height);
+		// blend the 2 images
+		differenceAccuracy(blendedData.data, sourceData.data, lastImageData.data);
+		// draw the result in a canvas
+		contextBlended.putImageData(blendedData, 0, 0);
+		// store the current webcam image
+		lastImageData = sourceData;
+  };
+
+  fastAbs = (value) => {
+		// funky bitwise, equal Math.abs
+		return (value ^ (value >> 31)) - (value >> 31);
+  };
+  
+  threshold = (value) => { // return white or black
+		return (value > 0x15) ? 0xFF : 0;
+  };
+  
+  difference = (target, data1, data2) => {
+		// blend mode difference
+		if (data1.length != data2.length) return null;
+		let i = 0;
+		while (i < (data1.length * 0.25)) {
+			target[4*i] = data1[4*i] == 0 ? 0 : fastAbs(data1[4*i] - data2[4*i]);
+			target[4*i+1] = data1[4*i+1] == 0 ? 0 : fastAbs(data1[4*i+1] - data2[4*i+1]);
+			target[4*i+2] = data1[4*i+2] == 0 ? 0 : fastAbs(data1[4*i+2] - data2[4*i+2]);
+			target[4*i+3] = 0xFF;
+			++i;
+		}
+  };
+  
+  differenceAccuracy = (target, data1, data2) => {
+		if (data1.length != data2.length) return null;
+		let i = 0;
+		while (i < (data1.length * 0.25)) {
+			let average1 = (data1[4*i] + data1[4*i+1] + data1[4*i+2]) / 3;
+			let average2 = (data2[4*i] + data2[4*i+1] + data2[4*i+2]) / 3;
+			let diff = threshold(fastAbs(average1 - average2));
+			target[4*i] = diff;
+			target[4*i+1] = diff;
+			target[4*i+2] = diff;
+			target[4*i+3] = 0xFF;
+			++i;
+		}
+  };
+  
+  checkAreas = () => {
+		// loop over the note areas
+		for (let r=0; r<8; ++r) {
+			let blendedData = this.contextBlended.getImageData(1/8*r*video.width, 0, video.width/8, 100);
+			let i = 0;
+			let average = 0;
+			// loop over the pixels
+			while (i < (blendedData.data.length * 0.25)) {
+				// make an average between the color channel
+				average += (blendedData.data[i*4] + blendedData.data[i*4+1] + blendedData.data[i*4+2]) / 3;
+				++i;
+			}
+			// calculate an average between of the color values of the note area
+			average = Math.round(average / (blendedData.data.length * 0.25));
+			if (average > 10) {
+				console.log("BING")
+			}
+		}
+	}
 
   componentDidMount() {
     this.connectToEasyRTC();
+    this.update();
   }
 
   render() {
