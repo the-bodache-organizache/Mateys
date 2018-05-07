dc4660e7620daad4c49b778fb167a09a32e9f00b
Tyler Chan <TcFlux@gmail.com>
2018-05-07 16:13:32 -0400

    swabbed the poop deck

--- client/components/test/MotionDetection.js
+++ client/components/test/MotionDetection.js
@@ -60,7 +60,9 @@ class MotionDetection extends React.Component {
       timeOut: 0,
       lastImageData: {
         data: []
-      }
+      },
+      widgets: [null, null, {name: 'cannon', command: 'The cannon needs to be loaded!'}, null, {name: 'poopDeck', command: 'Quickly, Swab the poop deck!'}, null]
+
     };
   }
 
@@ -151,13 +153,22 @@ class MotionDetection extends React.Component {
     }
   };
 
+  // for (let r = 3; r < 6; ++r) {
+  //   let sx = width - 50,
+  //       sy = 1 / 3 * (r - 3) * height,
+  //       sw = 50,
+  //       sh = height * .3
+  //   if (r >= 4) {
+  //     sy = (1 / 3 * (r - 3) * height) + (height * .05);
+  //   }
+
   checkAreas = () => {
     // loop over the note areas
     let { width, height } = this;
     width = +width;
     height = +height;
     const { contextBlended } = this.state;
-    for (let r = 0; r < 3; ++r) {
+    for (let r = 0; r < 6; ++r) {
       let sx = 0,
           sy = 1 / 3 * r * height,
           sw = 50,
@@ -165,31 +176,10 @@ class MotionDetection extends React.Component {
       if (r >= 1) {
         sy = (1 / 3 * r * height) + (height * .05);
       }
-      let blendedData = contextBlended.getImageData(sx, sy, sw, sh);
-      let i = 0;
-      let average = 0;
-      // loop over the pixels
-      while (i < blendedData.data.length * 0.25) {
-        // make an average between the color channel
-        average +=
-          (blendedData.data[i * 4] +
-            blendedData.data[i * 4 + 1] +
-            blendedData.data[i * 4 + 2]) /
-          3;
-        ++i;
+      if (r >= 3) {
+        sx = width - 50;
+        sy = 1 / 3 * (r - 3) * height;
       }
-      // calculate an average between of the color values of the note area
-      average = Math.round(average / (blendedData.data.length * 0.25));
-      if (average > 10) {
-        console.log("BING", r)
-        this.socket.emit('press box', {});
-      }
-    }
-    for (let r = 3; r < 6; ++r) {
-      let sx = width - 50,
-          sy = 1 / 3 * (r - 3) * height,
-          sw = 50,
-          sh = height * .3
       if (r >= 4) {
         sy = (1 / 3 * (r - 3) * height) + (height * .05);
       }
@@ -209,8 +199,11 @@ class MotionDetection extends React.Component {
       // calculate an average between of the color values of the note area
       average = Math.round(average / (blendedData.data.length * 0.25));
       if (average > 10) {
-        console.log("BING", r)
-        this.socket.emit('press box', {});
+        let widget = this.state.widgets[r]
+        if (widget) {
+          console.log(widget.name);
+          this.socket.emit('press box', widget);
+        }
       }
     }
   };
