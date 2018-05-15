bbcd6ad9c9d8cc6e3cf30f41105cd9e3d8108529
Tyler Chan <TcFlux@gmail.com>
2018-05-15 11:12:46 -0400

    change opacity on widget hit

--- client/components/game/Widget.js
+++ client/components/game/Widget.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const Widget = props => {
-  const { index, widget, height, color } = props;
+  const { index, widget, height, opacity } = props;
   const widgetStyle = {
     height: +height * 0.3,
     margin: +height * 0.025,
-    border: `solid 2px ${color}`
+    opacity: opacity
   };
 
   return (
